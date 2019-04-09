29fd23485d3774bf614740ace5dff876b7b074b6
adrianvillanueva997 <adrianvillanueva997@gmail.com>
2019-04-09 21:24:43 +0200

    Actualizacion de dependencias y el readme

--- dependencias.sh
+++ dependencias.sh
@@ -2,5 +2,5 @@
 
 echo "Gracias por utilizar el instalador de dependencias!";
 echo "A continuación se instalarán las siguientes librerías:";
-echo "numpy pandas matplotlib graphviz sklearn nltk bs4 sqlalchemy";
-python3 -m pip install numpy pandas matplotlib graphviz sklearn nltk bs4 sqlalchemy mysqlclient
+echo "numpy pandas matplotlib graphviz sklearn nltk bs4 sqlalchemy vadersentiment";
+python3 -m pip install numpy pandas matplotlib graphviz sklearn nltk bs4 sqlalchemy mysqlclient vadersentiment
